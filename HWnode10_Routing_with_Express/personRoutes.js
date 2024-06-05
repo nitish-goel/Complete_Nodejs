4e4adb4d93d53717295c5eb4edadff3e0682f9ec
Nitish Goel <nitishgoel20@gmail.com>
2024-06-05 18:21:26 +0530

    Added update & delete routes in person

--- HWnode10_Routing_with_Express/personRoutes.js
+++ HWnode10_Routing_with_Express/personRoutes.js
@@ -1,3 +1,6 @@
+// Update and Delete Method 
+// Adding Updation and Deletion End-Point  in Person-Routes
+
 const express = require('express');
 const router = express.Router();
 const Person = require('./../HWnode8_models/Person');
@@ -5,7 +8,6 @@ const Person = require('./../HWnode8_models/Person');
 
 
 // POST route to add a person
-// router.post('/person', async (req, res) => {
 router.post('/', async (req, res) => {
     try {
 
@@ -26,7 +28,6 @@ router.post('/', async (req, res) => {
 
 })
 
-// router.get('/person', async (req, res) => {
 router.get('/', async (req, res) => {
     try {
         const data = await Person.find();
@@ -39,7 +40,7 @@ router.get('/', async (req, res) => {
     }
 })
 
-// router.get('/person/:workType', async (req, res) => {  //http://localhost:3000/person/waiter --> parameterized url
+// Parameterized End-Point
 router.get('/:workType', async (req, res) => {  //http://localhost:3000/person/waiter --> parameterized url
     try {
         const workType = req.params.workType; // Extract the worktype from the url parameter
@@ -51,6 +52,53 @@ router.get('/:workType', async (req, res) => {  //http://localhost:3000/person/w
             res.status(404).json({ Error: "Invalid work-Type" });
         }
     } catch (err) {
+        console.log(err);
+        res.status(500).json({ Error: "Internal Server Error" })
+    }
+})
+
+// Updation End-Point / Person Updation Method : we can use both PUT and PATCH for updation
+router.put('/:id', async (req, res) => {
+    try {
+        const personId = req.params.id; // Extract the person's ID from the URL parameter
+        const updationData = req.body; // data for updation
+
+        const response = await Person.findByIdAndUpdate(personId, updationData, {
+            new: true, // Retrun  the updated document
+            runValidators: true, // Run Mongoose validation
+        })
+
+        
+        
+        if(!response){ // if id is not matched
+            return res.status(404).json({Error:"Person not Found"});
+        }
+        
+        console.log("Data is Updated");
+        res.status(200).json(response);
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ Error: "Internal Server Error" })
+    }
+
+})
+
+// Deletion End-Point / Person Deletion Method : we use DELETE for delete.
+router.delete('/:id',async (req,res)=>{
+    try{
+        const personId = req.params.id; // Extract the person's ID from the URL parameter
+        
+        // Assuming you have a Person model
+        const response = await Person.findByIdAndDelete(personId);
+        
+        if(!response){ // if id is not matched
+            return res.status(404).json({Error:"Person not Found"});
+        }
+        
+        console.log("Data is Deleted");
+        res.status(200).json({message :"Person deleted Sucessfully"});
+    }catch(err){
         console.log(err);
         res.status(500).json({ Error: "Internal Server Error" })
     }
--- HWnode10_Routing_with_Express/server.js
+++ HWnode10_Routing_with_Express/server.js
@@ -10,9 +10,6 @@ const db = require('../HWnode8_db'); // import database file for connection.
 const bodyParser = require('body-parser'); // importing middleware to extract body from http request.
 app.use(bodyParser.json()); // To use middleware we use the app.use().
 
-// we import Person named model 
-// const Person = require('../HWnode8_models/Person');
-
 // import the routes of Person 
 const PersonRoutes = require('./personRoutes');
 
@@ -24,54 +21,6 @@ app.get('/', function (req, res) {
     res.send("Hello This is Coder's World");
 })
 
-// // POST route to add a person
-// app.post('/person', async ( req, res)=>{
-//        try{
-
-//         const data = req.body; // Assuming the request body contains the person data.
-    
-//         // Create a new Person document using the Mongoose model
-//         const newPerson = new Person(data);
-    
-//         // Save the new person to the database
-//         const response = await newPerson.save();
-//         console.log("Data saved");
-//         res.status(200).json(response);
-
-//     }catch(err){
-//         console.log(err);
-//         res.status(500).json({error:"Internal Server Error"});
-//     }
-     
-// })
-
-// app.get('/person',async (req,res)=>{
-//     try{
-//         const data = await Person.find();
-//         console.log("Data fetched");
-//         res.status(200).json(data);
-        
-//     }catch(err){
-//         console.log(err);
-//         res.status(500).json({error :" Internal server error"})
-//     }
-// })
-
-// app.get('/person/:workType',async (req,res)=>{  //http://localhost:3000/person/waiter --> parameterized url
-//     try{
-//         const workType = req.params.workType; // Extract the worktype from the url parameter
-//         if(workType == 'chef' || workType == 'manager' || workType == 'waiter'){
-//             const response = await Person.find({ work: workType})
-//             console.log("Response Fetched");
-//             res.status(200).json(response);
-//         }else{
-//             res.status(404).json({Error:"Invalid work-Type"});
-//         }
-//     }catch(err){
-//         console.log(err);
-//         res.status(500).json({Error:"Internal Server Error"})
-//     }
-// })
 
 // Accessing Person routes
 app.use('/person', PersonRoutes);
